0c5c98ee337add1e46bebc6fc236761a7907ebd7
jyliao369 <jyliao369@gmail.com>
2022-03-28 14:04:39 -0400

    adjusted the create posting and the functionality works as well

--- public/js/posting.js
+++ public/js/posting.js
@@ -1,31 +1,42 @@
 const newPostHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('#title').value.trim();
-    const category = document.querySelector('#category').value.trim();
-    const system = document.querySelector('#system').value.trim();
-    const genre = document.querySelector('#genre').value.trim();
-    const condition = document.querySelector('#condition').value.trim();
-    const price = document.querySelector('#price').value.trim();
-    const description = document.querySelector('#desc').value.trim();
-  
-    if (title && category && condition && price && description) {
-      const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({ title, category, system, genre, condition, price, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create project');
-      }
+  const title = document.querySelector(".product").value.trim();
+  const category = document.querySelector(".publisher").value.trim();
+  const system = document.querySelector(".system").value.trim();
+  const genre = document.querySelector(".genre").value.trim();
+  const condition = document.querySelector(".condition").value.trim();
+  const price = document.querySelector(".price").value.trim();
+  const description = document.querySelector(".desc").value.trim();
+
+  console.log(title);
+  console.log(description);
+
+  if (title && category && condition && price && description) {
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        category,
+        system,
+        genre,
+        condition,
+        price,
+        description,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create project");
     }
-  };
+  }
+};
 
-  document
-    .querySelector('.posting-form')
-    .addEventListener('submit', newPostHandler);
+document
+  .querySelector(".createPost")
+  .addEventListener("submit", newPostHandler);
